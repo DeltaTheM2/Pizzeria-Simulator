9f6da15dab701808a0f1aae1231769b21f471275
susSusboi69 <135538676+susSusboi69@users.noreply.github.com>
2023-10-22 12:14:54 -0700

    Traffic Update
    Co-Authored-By: DeltaTheM2 <72804931+DeltaTheM2@users.noreply.github.com>

--- Assets/Scripts/TrafficManager.cs
+++ Assets/Scripts/TrafficManager.cs
@@ -1,21 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TrafficManager : MonoBehaviour
 {
  public List<GameObject> carsPrefab = new List<GameObject>();
     public GameObject[] startingPoints, finishPoints;
+    public float timePassed = 0;
 
     public void SpawnCars()
     {
         int randomIndex = Random.Range(0, startingPoints.Length);
-        if (startingPoints[randomIndex].GetComponent<SpawnTrigger>().isFull())
+        if (!startingPoints[randomIndex].GetComponent<SpawnTrigger>().isFull())
         {
             GameObject randomCars = carsPrefab[Random.Range(0, carsPrefab.Count)];
 
             GameObject currentStart = startingPoints[randomIndex];
-            Instantiate(randomCars, currentStart.transform.position, Quaternion.identity);
+            Instantiate(randomCars, currentStart.transform.position, Quaternion.LookRotation(finishPoints[randomIndex].transform.position - startingPoints[randomIndex].transform.position, Vector3.forward));
             randomCars.GetComponent<FollowTraffic>().finishPoint = finishPoints[randomIndex];
         }
         else
@@ -24,9 +26,21 @@ public class TrafficManager : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        timePassed += Time.deltaTime;
+        if(timePassed > 5) {
+            SpawnCars();
+            timePassed = 0;
+        }
+    }
 
-
-
+    IEnumerator WaitForSpawn()
+    {
+        yield return new WaitForSeconds(5);
+        SpawnCars();
+        timePassed = 0;
+    }
 
 
 }
